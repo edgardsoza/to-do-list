4f242629f76fbd8ce189e691e383a41909184f54
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-03 17:48:57 -0600

    last functional version

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -22,47 +22,35 @@ function addtodolist () {
     for (let i = 0 ; i < todolist.length; i += 1) {
         const listitem = document.createElement('li');
         const inputitem = document.createElement('input');
+        inputitem.value = todolist[i].completed;
         inputitem.classList.add('checkbox');
         inputitem.setAttribute('id', 'checkbox');
         inputitem.setAttribute('type', 'checkbox');
-        inputitem.value = todolist[i].completed;
-        const inputtodo = document.createElement('input');
+        const inputtodo = document.createElement('label');
+        inputtodo.textContent = todolist[i].description;
         inputtodo.setAttribute('type', 'text');
         inputtodo.setAttribute('id', 'todotask');
-        inputtodo.setAttribute('onclick', 'changetodo(id)');
         inputtodo.classList.add('todotask');
-        inputtodo.textContent = todolist[i].description;
         const indexid = document.createElement('span');
-        indexid.classList.add('material-symbols-outlined');
         indexid.setAttribute('id', todolist[i].index);
+        indexid.classList.add('material-symbols-outlined');
         indexid.classList.add('trash');
         indexid.setAttribute('onclick', 'removetodo(id)');
         indexid.textContent = 'delete';
         listitem.append(inputitem, inputtodo, indexid);
         interactivelist.appendChild(listitem);
     }
-    console.log(todolist)
   };
 
   const reassignedindex = (filteredArray) => {
     filteredArray.forEach((item, i) => {
-    item.index = i +1;
+    item.index = i + 1 ;
     })};
-
-window.changetodo = (id) => {
-    const inputtodo = document.getElementById('todotask').value;
-    const updateArray = todolist.map((item) => {
-        if (item.index - 1 === id) {
-          item.description = inputtodo;
-        }});
-    localStorage.setItem('newtask', JSON.stringify(updateArray));
-};
 
 window.removetodo = (id) => {
   const filteredArray = todolist.filter(todo => todo != todolist[id-1]);
   reassignedindex(filteredArray);
   localStorage.setItem('newtask', JSON.stringify(filteredArray));
-  console.log(filteredArray);
   displaylist();
 }
 
