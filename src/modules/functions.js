b4769ce74666c7a421f85e6df936313d3a98f487
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-04 11:54:31 -0600

    fixing linter errors

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -12,61 +12,60 @@ function addtodolist () {
     descriptioninput.value = '';
   };
   
-  function displaylist() {
-    if (localStorage) {
-        todolist = JSON.parse(localStorage.newtask);}
-          else {
-        todolist = [];
-        };
-        console.log(todolist);
-    const interactivelist = document.querySelector('.interactive-list');
-    interactivelist.textContent = '';
-    for (let i = 0 ; i < todolist.length; i += 1) {
-        const listitem = document.createElement('li');
-        const inputitem = document.createElement('input');
-        inputitem.value = todolist[i].completed;
-        inputitem.classList.add('checkbox');
-        inputitem.setAttribute('id', 'checkbox');
-        inputitem.setAttribute('type', 'checkbox');
-        const inputtodo = document.createElement('input');
-        inputtodo.setAttribute('onchange', 'changetodo(id)');
-        inputtodo.setAttribute('type', 'text');
-        inputtodo.setAttribute('id', todolist[i].index);
-        inputtodo.value = todolist[i].description;
-        inputtodo.classList.add('todotask');
-        const indexid = document.createElement('span');
-        indexid.setAttribute('id', todolist[i].index);
-        indexid.classList.add('material-symbols-outlined');
-        indexid.classList.add('trash');
-        indexid.setAttribute('onclick', 'removetodo(id)');
-        indexid.textContent = 'delete';
-        listitem.append(inputitem, inputtodo, indexid);
-        interactivelist.appendChild(listitem);
-    }
-  };
+function displaylist() {
+if (localStorage) {
+    todolist = JSON.parse(localStorage.newtask);}
+        else {
+    todolist = [];
+    };
+const interactivelist = document.querySelector('.interactive-list');
+interactivelist.textContent = '';
+for (let i = 0 ; i < todolist.length; i += 1) {
+    const listitem = document.createElement('li');
+    const inputitem = document.createElement('input');
+    inputitem.value = todolist[i].completed;
+    inputitem.classList.add('checkbox');
+    inputitem.setAttribute('id', 'checkbox');
+    inputitem.setAttribute('type', 'checkbox');
+    const inputtodo = document.createElement('input');
+    inputtodo.setAttribute('onchange', 'changetodo(id)');
+    inputtodo.setAttribute('type', 'text');
+    inputtodo.setAttribute('id', todolist[i].index);
+    inputtodo.value = todolist[i].description;
+    inputtodo.classList.add('todotask');
+    const indexid = document.createElement('span');
+    indexid.setAttribute('id', todolist[i].index);
+    indexid.classList.add('material-symbols-outlined');
+    indexid.classList.add('trash');
+    indexid.setAttribute('onclick', 'removetodo(id)');
+    indexid.textContent = 'delete';
+    listitem.append(inputitem, inputtodo, indexid);
+    interactivelist.appendChild(listitem);
+}
+};
 
 const reassignedindex = (filteredArray) => {
   filteredArray.forEach((item, i) => {
   item.index = i + 1 ;
   })};
 
-  window.changetodo = (id) => {
-    const updatetodo = document.getElementById(`${id}`).value;
-    const updatearray = todolist.map((item) => {
-      if (item.index == id) {
-        item.description = updatetodo;
-      }
-      return item;
-    });
-    localStorage.setItem('newtask', JSON.stringify(updatearray));
-    displaylist();
+window.changetodo = (id) => {
+const updatetodo = document.getElementById(`${id}`).value;
+const updatearray = todolist.map((item) => {
+    if (item.index == id) {
+    item.description = updatetodo;
+    }
+    return item;
+});
+localStorage.setItem('newtask', JSON.stringify(updatearray));
+displaylist();
 };
 
 window.removetodo = (id) => {
-  const filteredArray = todolist.filter(todo => todo != todolist[id-1]);
+  const filteredArray = todolist.filter(todo => todo !== todolist[id - 1]);
   reassignedindex(filteredArray);
   localStorage.setItem('newtask', JSON.stringify(filteredArray));
   displaylist();
-}
+};
 
-  export {displaylist, addtodolist};
+export { displaylist, addtodolist };
