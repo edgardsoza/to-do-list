b85fe927dd8a38c1966297d3c24bb0826f0a8053
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-08 17:43:30 -0600

    fixing linter errors

--- src/modules/to-do.js
+++ src/modules/to-do.js
@@ -1,41 +1,43 @@
+const todolist = [];
+
 function localStorageSet() {
   todolist = JSON.parse(localStorage.newtask);
 }
-
-const todolist = [];
 
 function displaylist() {
   if (localStorage) {
     localStorageSet();
   } else {
   }
-const interactivelist = document.querySelector('.interactive-list');
-interactivelist.textContent = '';
-for (let i = 0; i < todolist.length; i += 1) {
-  const listitem = document.createElement('li');
-  const inputitem = document.createElement('input');
-  inputitem.checked = todolist[i].completed;
-  inputitem.classList.add('checkbox');
-  inputitem.setAttribute('id', todolist[i].index);
-  inputitem.setAttribute('type', 'checkbox');
-  inputitem.setAttribute('onchange', 'checkboxtodo(id)');
-  const inputtodo = document.createElement('input');
-  inputtodo.setAttribute('onchange', 'changetodo(id)');
-  inputtodo.setAttribute('type', 'text');
-  inputtodo.setAttribute('id', todolist[i].index);
-  inputtodo.value = todolist[i].description;
-  inputtodo.classList.add('todotask');
-  const indexid = document.createElement('span');
-  indexid.setAttribute('id', todolist[i].index);
-  indexid.classList.add('material-symbols-outlined');
-  indexid.classList.add('trash');
-  indexid.setAttribute('onclick', 'removetodo(id)');
-  indexid.textContent = 'delete';
-  listitem.append(inputitem, inputtodo, indexid);
-  interactivelist.appendChild(listitem);
-}
-}
+  const interactivelist = document.querySelector('.interactive-list');
+  interactivelist.textContent = '';
+  for (let i = 0; i < todolist.length; i += 1) {
+    const listitem = document.createElement('li');
+    const inputitem = document.createElement('input');
+    inputitem.checked = todolist[i].completed;
+    inputitem.classList.add('checkbox');
+    inputitem.setAttribute('id', todolist[i].index);
+    inputitem.setAttribute('type', 'checkbox');
+    inputitem.setAttribute('onchange', 'checkboxtodo(id)');
+    const inputtodo = document.createElement('input');
+    inputtodo.setAttribute('onchange', 'changetodo(id)');
+    inputtodo.setAttribute('type', 'text');
+    inputtodo.setAttribute('id', todolist[i].index);
+    inputtodo.value = todolist[i].description;
+    inputtodo.classList.add('todotask');
+    const indexid = document.createElement('span');
+    indexid.setAttribute('id', todolist[i].index);
+    indexid.classList.add('material-symbols-outlined');
+    indexid.classList.add('trash');
+    indexid.setAttribute('onclick', 'removetodo(id)');
+    indexid.textContent = 'delete';
+    listitem.append(inputitem, inputtodo, indexid);
+    interactivelist.appendChild(listitem);
+  }
+  }
 
-export function localStorageSave() {
+function localStorageSave() {
   localStorage.setItem('newtask', JSON.stringify(todolist));
 }
+
+export {localStorageSave, displaylist, localStorageSet};
