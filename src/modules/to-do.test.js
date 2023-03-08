284afb8e03bd5a91e2ac8bc0579125bbdd317ba8
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-08 17:23:31 -0600

    fixing linter errors

--- src/modules/to-do.test.js
+++ src/modules/to-do.test.js
@@ -28,34 +28,34 @@ window.removetodo = (id) => {
 describe('add and delete in to-do-list', () => {
     test('add something', () => {
     document.body.innerHTML = '<div class="input-items">'
-        +'<input type="text" id="task" name="task" placeholder="Add to your list..." required>'
-        +'<button type="submit" class="submit"><span class="material-symbols-sharp" id="enter">'
-        +'keyboard_return'
-        +'</span></button>'
-        +'</div>';
+        + '<input type="text" id="task" name="task" placeholder="Add to your list..." required>'
+        + '<button type="submit" class="submit"><span class="material-symbols-sharp" id="enter">'
+        + 'keyboard_return'
+        + '</span></button>'
+        + '</div>';
 
     descriptioninput = document.getElementById('task');
     addtodolist();
     expect(todolist.length).toBe(1);
   });
 
-   test('remove something', () => {
+  test('remove something', () => {
     document.body.innerHTML = '<div class="to-do-list">'
-        +'<div class="interactive-list">'
-        +'<li>' 
-        +'<input class="checkbox" id="1" type="checkbox" onchange="checkboxtodo(id)">' 
-        +'<input onchange="changetodo(id)" type="text" id="1" class="todotask">'
-        +'<span id="1" class="material-symbols-outlined trash" onclick="removetodo(id)">delete</span>'
-        +'</li>' 
-        +'<li>' 
-        +'<input class="ch" id="1" type="checkbox" onchange="checkboxtodo(id)">' 
-        +'<input onchange="changetodo(id)" type="text" id="1" class="todotask">'
-        +'<span id="1" class="material-symbols-outlined trash" onclick="removetodo(id)">delete</span>'
-        +'</li>' 
-        +'</div>'
-        +'</div>';
+        + '<div class="interactive-list">'
+        + '<li>'
+        + '<input class="checkbox" id="1" type="checkbox" onchange="checkboxtodo(id)">'
+        + '<input onchange="changetodo(id)" type="text" id="1" class="todotask">'
+        + '<span id="1" class="material-symbols-outlined trash" onclick="removetodo(id)">delete</span>'
+        + '</li>'
+        + '<li>'
+        + '<input class="ch" id="1" type="checkbox" onchange="checkboxtodo(id)">'
+        + '<input onchange="changetodo(id)" type="text" id="1" class="todotask">'
+        + '<span id="1" class="material-symbols-outlined trash" onclick="removetodo(id)">delete</span>'
+        + '</li>'
+        + '</div>'
+        + '</div>';
 
-    todolist = [{ completed: false, description: 'Play Soccer', index: 1 } , { completed: false, description: 'Play Soccer', index: 2 }];
+    todolist = [{ completed: false, description: 'Play Soccer', index: 1 }, { completed: false, description: 'Play Soccer', index: 2 }];
     const filteredArray1 = window.removetodo(1);
     expect(filteredArray1.length).toBe(1);
   });
